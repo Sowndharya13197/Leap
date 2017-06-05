6d402aaa27c4d079ce98d7a761e3d19a33119a3d
Sowndharya13197 <sowndharyagopal13@gmail.com>
2017-06-05 10:17:51 -0700

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+public class Largest {
+    public static void main(String args[])
+    {int i;
+        System.out.println("enter year");
+        Scanner s=new Scanner(System.in);
+        
+        
+          int a=s.nextInt();
+          if(a%4==0)
+          {
+        System.out.println("leap year"); 
+        }
+         else
+          {System.out.println("not leap");
+    }
+    
+    }}
